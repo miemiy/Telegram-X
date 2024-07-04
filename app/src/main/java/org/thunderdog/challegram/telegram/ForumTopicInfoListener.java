f5a84381c169a1377537ba13fb27be9e6f4086cf
neoo <annbeby@gmail.com>
2024-07-04 16:23:12 +0800

    Delete app directory

--- app/src/main/java/org/thunderdog/challegram/telegram/ForumTopicInfoListener.java
+++
@@ -1,7 +0,0 @@
-package org.thunderdog.challegram.telegram;
-
-import org.drinkless.tdlib.TdApi;
-
-public interface ForumTopicInfoListener {
-  default void onForumTopicInfoChanged (long chatId, TdApi.ForumTopicInfo info) { }
-}
